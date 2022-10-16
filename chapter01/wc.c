2891486fa7a62bc0c0675b96232bc2a9af2f34f7
Leon Ding <ding@ibyte.me>
2022-10-16 21:49:12 +0800

    add wc program.

---
+++ chapter01/wc.c
@@ -0,0 +1,41 @@
+// 统计单词数、行数、字符数
+#include <stdio.h>
+
+// 输入的状态转换
+#define IN 1
+#define OUT 0
+
+// 统计辅助转换符
+#define LINE   '\n'
+#define TABE   '\t'
+#define BLANK  ' '
+
+int main() {
+    
+    // 输入字符、行数、单词数、字符数、输入单词状态
+    int c, nl, nw, nc, state;
+
+    state = OUT;
+    // 可以简写初始化
+    nl = (nw = (nc = 0));
+
+    // ctr + d == EOF
+    while ((c = getchar()) != EOF ) {
+        ++nc;
+        if (c == LINE) {
+            ++nl;
+        }
+        if (c == LINE || c == TABE || c == BLANK) {
+            state = OUT;
+        } else if (state == OUT) {
+            state = IN;
+            ++nw;
+        }
+    }
+
+    printf("Lines: %d Words: %d Chars: %d \n",nl,nw,nc);
+
+    return 0;
+}
+
+
