0c6b81007aeff3eea91fca47cb803da8470be3da
Leon Ding <ding@ibyte.me>
2022-10-20 15:17:20 +0800

    add operators example year.

---
+++ chapter2/year.c
@@ -0,0 +1,24 @@
+/* 计算某个年份是否为润年  */
+#include <stdio.h>
+
+
+/* 年份变量  */
+int year = 0;
+
+/* 函数原型 */
+int checked(int year);
+
+
+int main() {
+	printf("input year: \n");
+	scanf("%d",&year);
+	printf("%d %s leap year \n",year,checked(year) > 0 ? "is" : "not");
+}
+
+int checked(int year) {
+	if ((year % 4 == 0 && year % 100 != 0) || year % 400 == 0) {
+		return 1;
+	} else {
+		return 0;
+	}
+}
