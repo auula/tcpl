251352cc39d8e8df9c651dda65a60896f2ea6079
auual <ding@ibyte.me>
2022-10-27 22:29:31 +0800

    add the c programming language.

---
+++ swap.c
@@ -0,0 +1,20 @@
+/* 编写一个函数交换两个变量的值 */
+#include <stdio.h>
+
+void swap(int *px,int *py);
+
+int main() {
+	int i = 10, j = 100;
+	printf("i = %d j = %d\n",i,j);
+	swap(&i,&j);
+	printf("i = %d j = %d\n",i,j);
+}
+
+
+int swap(int *px,int *py) {
+	int temp = 0;
+	
+	temp = *px;
+	px = *py;
+	py = *px;
+}
