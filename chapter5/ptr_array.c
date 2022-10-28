17d30b3ec84c11df8c89d2db91c58dfd226560b7
auual <ding@ibyte.me>
2022-10-28 15:18:12 +0800

    add chapter5 array.

---
+++ chapter5/ptr_array.c
@@ -0,0 +1,24 @@
+/* 通过指针的方式给外部数组赋值  */
+#include <stdio.h>
+
+
+#define SIZE 10
+
+
+void print_arr(int *pi);
+
+int main() {
+	int n, array[SIZE], getint(int *);
+	for (n = 0; n < SIZE ; n++) {
+		array[n] = n; 
+	}
+	print_arr(&array[0]);	
+	return 0;
+}
+
+
+void print_arr(int *pi) {
+	for (int i = 0; i < SIZE; i++) {
+		printf("array[%d] = %d\n",i,*pi+i * 10);
+	}
+}
