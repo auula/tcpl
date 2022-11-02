de6d155c25e305fcc43f68468104d779e2ef4652
auual <ding@ibyte.me>
2022-11-02 15:55:03 +0800

    add c structure.

--- chapter6/structure.c
+++ chapter6/structure.c
@@ -3,11 +3,12 @@
 
 struct Person {
 	unsigned short age;
-	char name[];
-}
+	char *name;
+};
 
 int main() {
-	
+	struct Person people = { 22, "Leon Ding" };
+	printf("people = { age: %d, name:%s }\n",people.age,people.name);	
 	return 0;
 }
 
