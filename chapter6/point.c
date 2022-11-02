0c192f8e49b1e27b53e1cded1abd1126a80e1d47
auual <ding@ibyte.me>
2022-11-02 21:57:02 +0800

    add c structure function pointer transfer.

--- chapter6/point.c
+++ chapter6/point.c
@@ -16,17 +16,17 @@ struct point* ptrpoint(struct point *pp);
 int main() {
 	struct point p1 = makepoint(10,20);
 	
-	printf("p.x = %d p.y = %d\n",p1.x,p1.y);
+	printf("p1.x = %d p1.y = %d\n",p1.x,p1.y);
 	
 	struct point p2 = {10,20};
 	
 	p1 = addpoint(p1,p2);
 
-	printf("p.x = %d p.y = %d\n",p1.x,p1.y);
+	printf("p1.x = %d p1.y = %d\n",p1.x,p1.y);
 	
 	struct point* p3 = ptrpoint(&p1);
 
-	printf("p.x = %d p.y = %d\n",(*p3).x,(*p3).y);
+	printf("p3.x = %d p3.y = %d\n",(*p3).x,(*p3).y);
 
 	return 0;
 }
@@ -47,7 +47,7 @@ struct point addpoint(struct point p1, struct point p2) {
 }
 
 struct point* ptrpoint(struct point *pp) {
-	pp->x *= 2;
-	pp->y *= 2;
+	pp->x *= pp->y;
+	pp->y *= pp->y;
 	return pp;
 }
