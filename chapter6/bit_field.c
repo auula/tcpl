0695103edcdfb6fcb0328063766a861c9c434c6d
auual <ding@ibyte.me>
2022-11-08 17:45:15 +0800

    add bit-field unsigned variable.

--- chapter6/bit_field.c
+++ chapter6/bit_field.c
@@ -3,13 +3,13 @@
 
 struct {
 	unsigned int is_keyword : 1;
-	unsigned int is_extren  : 1;
+	unsigned int is_extern  : 1;
 	unsigned int is_static  : 1;
 } flags;
 
 int main() {
-	flags.is_keyword = flags.is_extren = 1;
+	flags.is_keyword = flags.is_extern = 1;
 	flags.is_static = 0;
-	printf("flags.is_keyword = %d,flags.extren = %d,flags.static = %d\n",flags.is_keyword,flags.is_extren,flags.is_static);
+	printf("flags.is_keyword = %d,flags.extern = %d,flags.static = %d\n",flags.is_keyword,flags.is_extern,flags.is_static);
 	return 0;
 }
